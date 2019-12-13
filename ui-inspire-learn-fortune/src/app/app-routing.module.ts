00aef3e400eea9d7fb832abbafc39990161d57a3
Beth <ersergeant@gmail.com>
2019-12-12 19:44:06 -0500

    added flexbox

--- ui-inspire-learn-fortune/src/app/app-routing.module.ts
+++ ui-inspire-learn-fortune/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FortunePageComponent } from './fortune-page/fortune-page.component';
+import { InspirePageComponent } from './inspire-page/inspire-page.component';
+import { LearnPageComponent } from './learn-page/learn-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'fortune', component: FortunePageComponent},
+  { path: 'learn', component: LearnPageComponent},
+  { path: 'inspire', component: InspirePageComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui-inspire-learn-fortune/src/app/app.module.ts
+++ ui-inspire-learn-fortune/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -23,6 +25,7 @@ import { LearnPageComponent } from './learn-page/learn-page.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
