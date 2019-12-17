72919ea99ce5de9888707e120f334e796641392c
Beth <ersergeant@gmail.com>
2019-12-16 21:39:42 -0500

    added navbar

--- ui-inspire-learn-fortune/src/app/material.module.ts
+++ ui-inspire-learn-fortune/src/app/material.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import { 
+    MatButtonModule, 
+    MatIconModule,
+    MatToolbarModule, 
+} from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule],
-    exports: [MatButtonModule, MatIconModule]
+    imports: [
+        MatButtonModule, 
+        MatIconModule,
+        MatToolbarModule 
+    ],
+    exports: [
+        MatButtonModule, 
+        MatIconModule,
+        MatToolbarModule 
+    ]
 
 })
 export class MaterialModule {}
